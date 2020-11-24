232a9e1a7aa1cd67ac37601469e31124f04fd8e4
KangSoYeon <aaa0311@naver.com>
2020-11-24 09:26:03 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Login from './pages/Login';
+import DashBoard from './pages/DashBoard'
 
-function App() {
+
+function App () {
   return (
-    <div> temporary </div>
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Login}></Route>
+          <Route exact path="/main" component={DashBoard}></Route>
+        </Switch>
+    </Router>
   );
 }
 
